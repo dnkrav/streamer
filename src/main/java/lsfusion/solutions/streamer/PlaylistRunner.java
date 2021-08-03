bf87e83e47b5fab3100c00953c70e501ae455b2b
Dmitry Kravtsov <dnkrav@gmail.com>
2021-08-03 23:25:07 +0200

    Upgrade the Platform version to 5.0

--- src/main/java/lsfusion/solutions/streamer/PlaylistRunner.java
+++ src/main/java/lsfusion/solutions/streamer/PlaylistRunner.java
@@ -1,14 +1,7 @@
-package lsfusion.streamer;
+package lsfusion.solutions.streamer;
 
 import lsfusion.base.ExceptionUtils;
-import lsfusion.base.col.MapFact;
-import lsfusion.base.col.interfaces.immutable.ImMap;
-import lsfusion.base.col.interfaces.immutable.ImOrderMap;
-import lsfusion.base.col.interfaces.immutable.ImRevMap;
-import lsfusion.server.data.expr.key.KeyExpr;
-import lsfusion.server.data.query.build.QueryBuilder;
 import lsfusion.server.data.sql.exception.SQLHandledException;
-import lsfusion.server.data.value.ObjectValue;
 import lsfusion.server.language.ScriptingLogicsModule;
 import lsfusion.server.logics.action.controller.context.ExecutionContext;
 import lsfusion.server.logics.classes.ValueClass;
@@ -16,7 +9,6 @@ import lsfusion.server.logics.property.classes.ClassPropertyInterface;
 import lsfusion.server.physics.dev.integration.internal.to.InternalAction;
 import lsfusion.server.data.value.DataObject;
 
-import java.io.InputStream;
 import java.io.IOException;
 import java.lang.ProcessBuilder;
 import java.sql.SQLException;
@@ -47,12 +39,10 @@ public class PlaylistRunner extends InternalAction {
         }
     }
 
-    private void runPlaylist(ExecutionContext<ClassPropertyInterface> context, DataObject commandArgs) throws IOException, SQLException {
-        try (ExecutionContext.NewSession<ClassPropertyInterface> newContext = context.newSession()) {
-
-            // Initialise command
-            List<String> params = Arrays.asList(new String[]{
-                    "ffmpeg",
+    private void runPlaylist(ExecutionContext<ClassPropertyInterface> context, DataObject commandArgs) throws IOException {
+        try {
+            // Initialise command arguments, hardcoded for flv stream
+            List<String> params = Arrays.asList("ffmpeg",
                     // Hardcode parameters for now
                     // Read input at native frame rate
                     "-re",
@@ -70,29 +60,7 @@ public class PlaylistRunner extends InternalAction {
                     "-f","flv",
                     // Adding the RTMP resource
                     // rtmp://a.rtmp.youtube.com/live2 + / + stream key
-                    (String) findProperty("rtmp").read(context)
-                    });
-
-            // ToDo Gather parameters
-            /*
-            // Communicate with the table parameter of the CLASS Parameter directly
-            KeyExpr paramExpr = new KeyExpr("parameter");
-            ImRevMap<Object, KeyExpr> paramKeys = MapFact.singletonRev((Object) "parameter", paramExpr);
-            QueryBuilder<Object, Object> paramQuery = new QueryBuilder<>(paramKeys);
-            // select properties to be read from the database
-            paramQuery.addProperty("name", LM.findProperty("name[Parameter]").getExpr(context.getModifier(), paramExpr));
-            //paramQuery.addProperty("value", LM.findProperty("value[Parameter]").getExpr(paramExpr));
-            // Execute query to the database
-            //ImOrderMap<ImMap<Object, DataObject>, ImMap<Object, ObjectValue>> paramResult = paramQuery.execute(context);
-            ImOrderMap<ImMap<Object, Object>, ImMap<Object, Object>> paramResult = paramQuery.execute(context);
-            // Populate the parameters list
-            for (int key = 0, size = paramResult.size(); key < size; key++) {
-            //for (ImMap<Object, Object> key : paramResult.keyIt()) {
-                ImMap<Object, Object> values = paramResult.getValue(key);
-                params.add((String) values.get("name"));
-                //params.add((String) paramResult.get(key).get("value").getValue());
-            }
-             */
+                    (String) findProperty("rtmp").read(context));
 
             // Initialize and run the ffmpeg command
             ProcessBuilder runner = new ProcessBuilder(params);
